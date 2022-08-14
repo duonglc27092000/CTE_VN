3c37bd07c1f48d7710b1dcc3786e6d75dd77132c
LuongDuong99 <luongduongit99@gmail.com>
2022-08-14 13:56:08 +0700

    done toggle icon menu & link content

--- script.js
+++ script.js
@@ -1,16 +1,42 @@
 const $ = document.querySelector.bind(document);
 const $$ = document.querySelectorAll.bind(document);
 
-//toggle icon menu
-const tab1s = $$(".nav__all-btn");
+const closeIcon = $(".header__navbar-menu-bars--close");
+const menuIcon = $(".header__navbar-menu-bars--menu");
+// const elements = $$(".content div h1");
 
-tab1s.forEach((tab, index) => {
+// elements.forEach((element) => {
+//   element.scrollIntoView({
+//     behavior: "smooth",
+//     block: "start",
+//     inline: "nearest",
+//   });
+// });
+
+//toggle icon menu_________________
+closeIcon.onclick = function () {
+  menuIcon.classList.toggle("hide");
+  closeIcon.classList.toggle("hide");
+};
+menuIcon.onclick = function () {
+  menuIcon.classList.toggle("hide");
+  closeIcon.classList.toggle("hide");
+};
+
+// hide menu then click link content
+const link_lis = $$(".nav__all-list li");
+
+link_lis.forEach((tab, index) => {
   tab.onclick = function () {
-    $(".nav__all-btn.hide").classList.remove("hide");
-    this.classList.add("hide");
+    $("#nav__input").checked = false;
+    menuIcon.classList.toggle("hide");
+    closeIcon.classList.toggle("hide");
   };
 });
-// toggle icon menu END
+// ____________________________________
+
+// toggle icon menu END_______________
+
 // function d7() {
 //   tabs.forEach((tab, index) => {
 //     tab.onclick = function () {
@@ -27,21 +53,21 @@ const line = $(".tabs .line");
 
 // SonDN fixed - Active size wrong size on first load.
 // Original post: https://www.facebook.com/groups/649972919142215/?multi_permalinks=1175881616551340
-requestIdleCallback(function() {
-    line.style.left = tabActive.offsetLeft + "px";
-    line.style.width = tabActive.offsetWidth + "px";
+requestIdleCallback(function () {
+  line.style.left = tabActive.offsetLeft + "px";
+  line.style.width = tabActive.offsetWidth + "px";
 });
 tabs.forEach((tab, index) => {
-    const pane = panes[index];
+  const pane = panes[index];
 
-    tab.onclick = function() {
-        $(".tab-item.active").classList.remove("active");
-        $(".tab-pane.active").classList.remove("active");
+  tab.onclick = function () {
+    $(".tab-item.active").classList.remove("active");
+    $(".tab-pane.active").classList.remove("active");
 
-        line.style.left = this.offsetLeft + "px";
-        line.style.width = this.offsetWidth + "px";
+    line.style.left = this.offsetLeft + "px";
+    line.style.width = this.offsetWidth + "px";
 
-        this.classList.add("active");
-        pane.classList.add("active");
-    };
+    this.classList.add("active");
+    pane.classList.add("active");
+  };
 });
